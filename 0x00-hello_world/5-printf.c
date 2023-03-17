8e7fe888c4eec6b2567b03a96ed3be21e10f9d0f
Abdallah Hussien <abdallah.hussien101@gmail.com>
2023-03-16 19:52:40 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,13 @@
 /**
  * main - Entry point
  *
- * Description: program print with printf not pust
+ * Description: First program
  *
- * Return Always 0 (success)
- */
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
