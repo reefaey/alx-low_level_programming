b694064223c17680e6dadd65fbd795e5be9076b2
Abdallah Hussien <abdallah.hussien101@gmail.com>
2023-03-17 19:16:15 -0700

    first2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * main - entry point
  *
- * Description - practise task
+ * Description: false or true validation
  *
- * Return: Always 0 (Success)
- */
+ * Return: 0 (success)
+*/
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* code start there */
 	if (n > 0)
-	{
-	printf("%d is positive\n", n);
-	}
-	if (n < 0)
-	{
-	printf("%d is negative\n", n);
-	}
-	if (n == 0)
-	{
-	printf("%d is zero\n", n);
-	}
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
 	return (0);
 }
-
