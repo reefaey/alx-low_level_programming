b95c3eb0bf08b1b0f9cdbae9ef6497897e238708
Abdallah Hussien <abdallah.hussien101@gmail.com>
2023-03-17 19:21:16 -0700

    fix task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: print alphabet in lowercase then in uppercase
+ * Description: print all aplhabet letters
  *
  * Return: Always 0 (Success)
 */
@@ -11,20 +11,11 @@
 int main(void)
 {
 	char ch = 'a';
-	char CH = 'A';
 
-	/*prints a - z*/
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
-	}
-
-	/*print A - Z*/
-	while (CH <= 'Z')
-	{
-		putchar(CH);
-		CH++;
 	}
 	putchar('\n');
 
