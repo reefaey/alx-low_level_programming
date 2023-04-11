d84d6063cd93dc1b5bc39fde08fa240ff2c1e5a3
reefaey <abdallah.hussien101@gmail.com>
2023-04-11 03:20:11 +0200

    fixing

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
+/**
+ * main - entry point
+ * Return: always (0) success
+*/
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker
+	\"\n\t- Kevin Mitnick");
+	return (0);
 }
