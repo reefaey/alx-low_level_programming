dc030c495140c076e1d33329ded5e1f0bf8a7372
reefaey <abdallah.hussien101@gmail.com>
2023-07-06 04:54:03 +0300

    betty fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void _print_rev_recursion - function that prints a string in reverse.
+ * _print_rev_recursion - function that prints a string in reverse.
  * @s: param
  * Return: void
  */
