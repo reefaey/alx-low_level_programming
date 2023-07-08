cc3715fcfe97fa16529f4adbed7d8c2a2a59b47f
reefaey <abdallah.hussien101@gmail.com>
2023-07-08 05:38:09 +0300

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stadio.h>
+
+/**
+ * main - print number of arguments
+ * @argc: the number of arguments
+ * @argv: the value of arguments
+ *  Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
