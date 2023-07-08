d067f71ba9edae75fd437a17bd15fe425b5881e9
reefaey <abdallah.hussien101@gmail.com>
2023-07-08 05:28:56 +0300

    0-whatsmyname.c-fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,9 @@
 #include "stadio.h"
-#include "main.h"
 
 /**
  * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
- *
  * Return: Always 0 (Success)
  */
 int main(int argc, char const *argv[])
